857cdecf7a58a59249a3956d9dc7adff6222a61b
Kelvin Mai <kelvin.mai002@gmail.com>
2019-02-23 11:16:11 -0500

    Removed unit tests, use e2e tests instead

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,14 @@ import { NestFactory } from '@nestjs/core';
 import 'dotenv/config';
 import { AppModule } from './app.module';
 
-async function bootstrap() {
+if (process.env.NODE_ENV === 'test') {
+  console.log('TESTING IN PROCESS');
+  console.log('production database', process.env.MONGO_URI);
+  process.env.MONGO_URI = process.env.MONGO_URI_TEST;
+  console.log('using database', process.env.MONGO_URI);
+}
+
+export async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
 }
--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,27 +1,32 @@
-import { Test, TestingModule } from '@nestjs/testing';
+import { HttpStatus } from '@nestjs/common';
 import 'dotenv/config';
 import * as request from 'supertest';
-import { AppModule } from './../src/app.module';
 
 // test constants
 process.env.MONGO_URI = 'mongodb://localhost/nest_test';
 
 describe('AppController (e2e)', () => {
-  let app;
+  let app = 'http://localhost:3000';
 
-  beforeEach(async () => {
-    const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
-    }).compile();
+  // beforeEach(async () => {
+  //   const moduleFixture: TestingModule = await Test.createTestingModule({
+  //     imports: [AppModule],
+  //   }).compile();
 
-    app = moduleFixture.createNestApplication();
-    await app.init();
-  });
+  //   app = moduleFixture.createNestApplication();
+  //   await app.init();
+  // });
 
   it('/ (GET)', () => {
-    return request(app.getHttpServer())
+    return request(app)
       .get('/')
       .expect(200)
       .expect('Hello World!');
+  });
+
+  it('/auth (GET)', () => {
+    return request(app)
+      .get('/auth')
+      .expect(HttpStatus.UNAUTHORIZED);
   });
 });
